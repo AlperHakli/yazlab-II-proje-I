3993430f6d633e5aff58b253d96a9c55a73e482f
Alper <Alperhakli2004@gmail.com>
2026-03-27 14:04:43 +0300

    feat: test enrichent and some refactoring
    test_dispatcher_enriches_with_user_id test added
    GOAL: test if dispatcher adds user_id parameter to response or not

--- build/lib/dispatcher/tests/conftest.py
+++ build/lib/dispatcher/tests/conftest.py
@@ -1,7 +1,7 @@
 from dispatcher.app.main import app
 import pytest
 from fastapi.testclient import TestClient
-from constants import PROTECTED_ENDPOINTS , BOOK_SERVICE_URL , BORROW_SERVICE_URL
+from constants import PROTECTED_ENDPOINTS
 
 @pytest.fixture
 def client():
@@ -9,23 +9,6 @@ def client():
     Dependency Injection denir amaç test fonksiyonlarında client i dışarıdan vererek fonksiyon bağımlılığını azaltmaktır
     """
     return TestClient(app=app)
-
-@pytest.fixture
-def bookendpoint():
-    """
-    Sabit olan book url sini döndürür
-    """
-    return  BOOK_SERVICE_URL
-
-@pytest.fixture
-def borrowendpoint():
-    """
-    Sabit olan borrow url sini döndürür
-    """
-    return  BORROW_SERVICE_URL
-
-
-
 
 @pytest.fixture
 def secured_endpoint_list():
---
+++ dispatcher/tests/test_enrichment.py
@@ -0,0 +1,45 @@
+import respx
+import httpx
+from constants import ENDPOINTS_AND_URLS
+import pytest
+
+@pytest.mark.parametrize("endpoint" , "target_url" , ENDPOINTS_AND_URLS)
+@respx.mock
+def test_dispatcher_enriches_with_user_id(client , endpoint , target_url):
+        """
+        seneryo: kullanıcı geçerli token ile /books yada /borrow endpointlerine istek atar
+        hedef: amaçlanan şey burada dispatcher in cevaba user_id yide eklemesi
+        ek not: burada amaç dispatcher ile mikroservis arasındaki bir olayı kontrol etmek
+        """
+
+        # Dispatcher mikroservis e gittiğinde sanki mikroservis doğru çalışıyomuş gibi davranılır
+        # Aslında dispatcher hiç mikroservis e gitmez sadece gidiyomuş gibi yapılıp belirlediğimiz cevap dönülür
+        # Bu sayede zaten bizim yazdığımız cevaptan farklı bişey dönerse dispatcher de hata var demektir
+        # Yani kısaca dispatcher e ilgili url geldiğinde hiç gitmeden hazır cevap döndürür
+        respx.get(target_url).mock(return_value=httpx.Response(200 , json={"data" : "original data"}))
+
+        headers = {"Authorization": "Bearer gecerli_token_123"}
+        response = client.get(endpoint , headers=headers)
+
+        #kontroller
+
+        # burada tekrardan 200 beklememizin sebebi dispatcher in kendisinde bir hata oluşursa hiç mikroservise gitmeden
+        # bozulabilir bu sebeple bunu önleriz
+        assert response.status_code == 200
+        data = response.json()
+
+
+
+        # dispatcher kodunda dönen mock dataya bir "id" parametresi eklenmesi gerekir normalde
+        # eğerki bir sorun olurda eklenmezse bu kısım bozulur ve sorun anlaşılır
+        assert "id" in data
+
+
+
+
+
+
+
+
+
+
